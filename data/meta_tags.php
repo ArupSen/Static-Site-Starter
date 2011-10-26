1f0d06cdeed41959951689f02986fb31a6caae8b
Arup Sen <arupsen48@gmail.com>
2011-10-26 20:12:51 +0100

    first commit

---
+++ data/meta_tags.php
@@ -0,0 +1,25 @@
+<?php
+// arrays of the meta tags for --site name here--
+// use the page file name as the array name
+// -- meta_tags.php --
+
+// title - 80 characters
+// description - 255 characters
+// keywords - 1024 characters
+
+$index = array(
+	'title' => '',
+	'description' => '',
+	'keywords' => '');
+	
+$about = array(
+	'title' => '',
+	'description' => '',
+	'keywords' => '');
+	
+$contact = array(
+	'title' => '',
+	'description' => '',
+	'keywords' => '');
+
+?>
