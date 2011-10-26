aa93644f6e9ebe108495f69e5b8d7ca45095f971
Arup Sen <arupsen48@gmail.com>
2011-10-26 20:08:08 +0100

    first commit

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+// -- contact.php --
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+
+
+<p><span>tel/fax:</span> +44 (0) 20 7221 1747<br />
+<span>Mobile:</span> +44 (0)7760 116 946
+</p>
+<p>Or email: <a href="mailto:" title="send me an e-mail">enquiries@mysite.com</a>
+</p>
+
+<?php 
+// filename, title, id
+// img_tag('','',''); ?>
+
+<?php footer(); ?>
