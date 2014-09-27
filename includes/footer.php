6103d52a2876ecafe4a20b1e6b63742e06da8338
Arup Sen <arupsen48@gmail.com>
2014-09-27 17:49:25 +0100

    used sections from HTML 5 boiler plate along with link to site specific js

--- includes/footer.php
+++ includes/footer.php
@@ -5,16 +5,23 @@
 </div><!-- end of contents -->
 <div id="footer"> </div>
 </div> <!-- end of wrap -->
-<!-- the Google Analytics tracker -->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker(""); // add the tracker ID
-pageTracker._trackPageview();
-} catch(err) {}</script>
-
+<?php
+// latest jquery and bootstrap js from cdn
+// this site's js delivered from the js folder
+?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="<?php echo JSPATH.'main.js'; ?></script>
+<?php
+//Google Analytics: change UA-XXXXX-X to be your site's ID.
+?>
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
 </body>
 </html>
