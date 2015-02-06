7ec36fcf0ebf8a76f7e896b6a9d9f6475a4d648c
Arup Sen <arupsen48@gmail.com>
2015-02-06 13:13:27 +0000

    fixed missing closing quote and angle bracket

--- includes/footer.php
+++ includes/footer.php
@@ -11,7 +11,7 @@
 ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="<?php echo JSPATH.'main.js'; ?></script>
+<script src="<?php echo JSPATH.'main.js'; ?>"></script>
 <?php
 //Google Analytics: change UA-XXXXX-X to be your site's ID.
 ?>
