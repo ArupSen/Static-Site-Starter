bcd90481c721fee845bb3ad3d107b988555750ee
Arup Sen <arupsen48@gmail.com>
2015-02-06 13:35:55 +0000

    initial commit

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+// -- 404.php --
+
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<h1 id="heading">Page not found, sorry. Why? It probably doesn't exist. Try the back button.</h1>
+
+
+<?php footer(); ?>
